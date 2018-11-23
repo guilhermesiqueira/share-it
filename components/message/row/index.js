9ce43bf3b5a2c75f5fed1c587cbff8e453e31e96
Guilherme Siqueira <guilhersiqueira@gmail.com>
2018-11-23 08:14:36 -0200

    Testing route on the MessageScreen

--- components/message/row/index.js
+++ components/message/row/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 import PropTypes from 'prop-types';
 
 import style from './style';
@@ -7,14 +7,24 @@ import style from './style';
 class RowMessage extends React.Component {
 
   render() {
+    const { navigation } = this.props;
+
+    navigateChat = () => {
+      navigation.navigate('Register');
+    } 
     return (
-      <View style={style.content}>
-        <Image source={this.props.image} style={style.image} />
+      <TouchableOpacity onPress={() => navigateChat()}>
+        <View style={style.content}>
+          <Image source={this.props.image} style={style.image}/>
         <View>
-          <Text style={style.title}>{this.props.name}</Text>
-          <Text style={style.body}>{this.props.message.substring(0,30)}...</Text>
+            <Text style={style.title}>{this.props.name}</Text>
+            <Text style={style.body}>{this.props.message.substring(0,30)}...</Text>
+          </View>
+          <TouchableOpacity onPress={() => navigateChat()} style={{marginTop: 20, width: 160, height: 40, backgroundColor: '#F3903D', borderColor: 'gray', borderWidth: 1, borderRadius: 10, alignItems: 'center', justifyContent: 'center'}}>
+            <Text style={{ fontSize: 16 }}>TESTE</Text>
+          </TouchableOpacity>
         </View>
-      </View>
+      </TouchableOpacity>
     );
   }
 };
